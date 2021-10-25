ad75fea1da9fdc9ddfbe80d6285557f4441f4c1c
Ivars Mednis <ivars.mednis@g-i.lv>
2021-10-25 19:01:17 +0300

    Update main.blade.php

--- src/MultiInput/views/admin/main.blade.php
+++ src/MultiInput/views/admin/main.blade.php
@@ -1,9 +1,13 @@
 <div class="@if (!empty($className)){{ $className }}@endif" data-attribute="{{ $attribute }}">
-    <div class="multiinput-header">
-        <div class="multiinput-title @if ($root) h5 @endif">{{ $title }}</div>
+    <div class="multiinput-header" style="margin-bottom: 4px;">
+        {{-- <div class="multiinput-title @if ($root) h5 @endif">{{ $title }}</div> --}}
+        <div ></div>
+        @if (!$root)
+        <div class="multiinput-title @if ($root) h5 hidden @endif">{{ $title }}</div>
+        @endif
         @if (empty($config['single-row']))
             <span class="multiinput-elem-add" title="{{ __('multiinput::admin.add-item') }}">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="text-white-50 me-2"><path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path></svg>
+                <svg width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class=""><path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path></svg>
             </span>
         @endif
     </div>

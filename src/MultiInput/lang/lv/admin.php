aa983490837288b87a420aeea865d2045a981256
Ivars Mednis <ivars.mednis@g-i.lv>
2022-10-30 11:25:17 +0200

    Add LV translations

---
+++ src/MultiInput/lang/lv/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'add-item' => 'Add Item',
+    'remove-item' => 'Remove Item',
+    'clone-item' => 'Clone Item',
+    'file-item-message' => 'Save document and continue!',
+];
